33b7cfa251b68ebf10963cd6e258a8dcfecc5de4
NestorDR <ndr1970@gmail.com>
2022-09-21 15:15:58 +0200

    Added: SharedSizeScope

---
+++ LearningWPF/UserControls/Start/SharedSizeScope.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Controls;
+
+namespace LearningWPF.UserControls.Start
+{
+    /// <summary>
+    /// Interaction logic for SharedSizeScope.xaml
+    /// </summary>
+    public partial class SharedSizeScope : UserControl
+    {
+        public SharedSizeScope()
+        {
+            InitializeComponent();
+        }
+
+        private void SetTrue(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Grid.SetIsSharedSizeScope(MainPanel, true);
+            TextBlock.Text = "IsSharedSizeScope Property is set to " + Grid.GetIsSharedSizeScope(MainPanel).ToString();
+        }
+
+        private void SetFalse(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Grid.SetIsSharedSizeScope(MainPanel, false);
+            TextBlock.Text = "IsSharedSizeScope Property is set to " + Grid.GetIsSharedSizeScope(MainPanel).ToString();
+        }
+    }
+}
