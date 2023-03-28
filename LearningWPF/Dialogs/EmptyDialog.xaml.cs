69919c7aaf0136e0567d95cd9a738ab942f2a31f
NestorDR <ndr1970@gmail.com>
2023-03-28 13:30:54 +0200

    Added: Border expands to fill space in grid cell

---
+++ LearningWPF/Dialogs/EmptyDialog.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using CommonLibrary.ViewModels;
+
+namespace LearningWPF.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for EmptyDialog.xaml
+    /// </summary>
+    public partial class EmptyDialog : UserControl
+    {
+        #region Private variables
+        private ViewModelBase _viewModel;
+        #endregion
+
+#pragma warning disable CS8618      // Allows private fields uninitialized
+        public EmptyDialog()
+#pragma warning restore CS8618
+        {
+            InitializeComponent();
+        }
+
+        private void EmptyDialog_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            _viewModel = (ViewModelBase)DataContext;
+        }
+    }
+}
