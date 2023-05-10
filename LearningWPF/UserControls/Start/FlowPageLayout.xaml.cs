db7eb3b672bce33fafb61db649827744251f4e88
NestorDR <ndr1970@gmail.com>
2023-05-10 10:45:39 +0200

    FlowPageLayout

---
+++ LearningWPF/UserControls/Start/FlowPageLayout.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace LearningWPF.UserControls.Start
+{
+    /// <summary>
+    /// Interaction logic for FlowPageLayout.xaml
+    /// </summary>
+    public partial class FlowPageLayout : UserControl
+    {
+        public FlowPageLayout()
+        {
+            InitializeComponent();
+        }
+
+        private void ReaderWidthText_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (this.Reader != null) this.Width = this.Reader.Width + 80;
+        }
+    }
+}
